de67768f171ee7e87bdf8c47555c613162f9d5d6
lamine540 <camaralamine540@gmai.com>
2021-02-24 09:46:39 +0000

    envoi repository

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TransactionRepository::class)
+ */
+class Transaction
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $montant;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateDepot;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateRetrait;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateAnnulation;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ttc;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fraisEtat;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fraisSysteme;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fraisEnvoi;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $fraisRetrait;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codeTransaction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDateDepot(): ?\DateTimeInterface
+    {
+        return $this->dateDepot;
+    }
+
+    public function setDateDepot(\DateTimeInterface $dateDepot): self
+    {
+        $this->dateDepot = $dateDepot;
+
+        return $this;
+    }
+
+    public function getDateRetrait(): ?\DateTimeInterface
+    {
+        return $this->dateRetrait;
+    }
+
+    public function setDateRetrait(\DateTimeInterface $dateRetrait): self
+    {
+        $this->dateRetrait = $dateRetrait;
+
+        return $this;
+    }
+
+    public function getDateAnnulation(): ?\DateTimeInterface
+    {
+        return $this->dateAnnulation;
+    }
+
+    public function setDateAnnulation(\DateTimeInterface $dateAnnulation): self
+    {
+        $this->dateAnnulation = $dateAnnulation;
+
+        return $this;
+    }
+
+    public function getTtc(): ?int
+    {
+        return $this->ttc;
+    }
+
+    public function setTtc(int $ttc): self
+    {
+        $this->ttc = $ttc;
+
+        return $this;
+    }
+
+    public function getFraisEtat(): ?int
+    {
+        return $this->fraisEtat;
+    }
+
+    public function setFraisEtat(int $fraisEtat): self
+    {
+        $this->fraisEtat = $fraisEtat;
+
+        return $this;
+    }
+
+    public function getFraisSysteme(): ?int
+    {
+        return $this->fraisSysteme;
+    }
+
+    public function setFraisSysteme(int $fraisSysteme): self
+    {
+        $this->fraisSysteme = $fraisSysteme;
+
+        return $this;
+    }
+
+    public function getFraisEnvoi(): ?int
+    {
+        return $this->fraisEnvoi;
+    }
+
+    public function setFraisEnvoi(int $fraisEnvoi): self
+    {
+        $this->fraisEnvoi = $fraisEnvoi;
+
+        return $this;
+    }
+
+    public function getFraisRetrait(): ?int
+    {
+        return $this->fraisRetrait;
+    }
+
+    public function setFraisRetrait(int $fraisRetrait): self
+    {
+        $this->fraisRetrait = $fraisRetrait;
+
+        return $this;
+    }
+
+    public function getCodeTransaction(): ?int
+    {
+        return $this->codeTransaction;
+    }
+
+    public function setCodeTransaction(int $codeTransaction): self
+    {
+        $this->codeTransaction = $codeTransaction;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsersRepository::class)
+ */
+class Users
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cni;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $archivage;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCni(): ?string
+    {
+        return $this->cni;
+    }
+
+    public function setCni(string $cni): self
+    {
+        $this->cni = $cni;
+
+        return $this;
+    }
+
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar($avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getArchivage(): ?bool
+    {
+        return $this->archivage;
+    }
+
+    public function setArchivage(bool $archivage): self
+    {
+        $this->archivage = $archivage;
+
+        return $this;
+    }
+}
